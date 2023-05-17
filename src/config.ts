78e7ec0b13cf27b81ac23158a2b636e30a6930ff
Dennis Zach <dennis.zach@teutloff.de>
2023-05-17 19:04:13 +0200

    chore: commit types

--- src/config.ts
+++ src/config.ts
@@ -25,18 +25,9 @@ export interface ChangelogConfig {
 const getDefaultConfig = () =>
   <ChangelogConfig>{
     types: {
-      feat: { title: "🚀 Enhancements", semver: "minor" },
+      feat: { title: "🚀 Features", semver: "minor" },
       perf: { title: "🔥 Performance", semver: "patch" },
-      fix: { title: "🩹 Fixes", semver: "patch" },
-      refactor: { title: "💅 Refactors", semver: "patch" },
-      docs: { title: "📖 Documentation", semver: "patch" },
-      build: { title: "📦 Build", semver: "patch" },
-      types: { title: "🌊 Types", semver: "patch" },
-      chore: { title: "🏡 Chore" },
-      examples: { title: "🏀 Examples" },
-      test: { title: "✅ Tests" },
-      style: { title: "🎨 Styles" },
-      ci: { title: "🤖 CI" },
+      fix: { title: "🐞 Bug Fixes", semver: "patch" },
     },
     cwd: null,
     from: "",
